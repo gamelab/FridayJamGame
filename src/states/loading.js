32ce947fb272e651a54b393ae644f61b7d7d88a0
zacharyf1991 <zachary.freiberg@gmail.com>
2014-08-22 16:48:30 +1200

    IMplemented new neon lives balls

--- src/states/loading.js
+++ src/states/loading.js
@@ -14,7 +14,7 @@ FridayGameJam.Loading.preload = function () {
     this.addSpriteSheet('ball', './assets/img/game/ball.png', 124, 125);
     this.addSpriteSheet('blue-paddle', './assets/img/game/light-blue-paddle.png', 92, 62);
     this.addSpriteSheet('red-paddle', './assets/img/game/orange-paddle.png', 92, 62);
-    this.addSpriteSheet('ball-hud', './assets/img/game/balls-hud.png', 8, 8);
+    this.addSpriteSheet('ball-hud', './assets/img/game/balls-hud-neon.png', 8, 8);
 
     //Start Screen
     this.addImage('title', './assets/img/game-title.png');
